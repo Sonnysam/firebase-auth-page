c770412ddab66881e2415d695f022867b0da56c8
Samuel Agbenyo <80902426+Sonnysam@users.noreply.github.com>
2022-06-01 17:12:08 +0000

    home and login screens added

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen options={{ headerShown: false }} name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
--- screens/Login.js
+++ screens/Login.js
@@ -1,12 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
 import React from 'react'
+// import KeyboardAvoidingView from 'react-native/Libraries/Components/Keyboard/KeyboardAvoidingView'
 
 export default function Login() {
   return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
+    <KeyboardAvoidingView
+        style={styles.container}
+        behavior="padding"
+    >
+      <View style={styles.inputCon}>
+          <TextInput
+            placeholder="Email"
+            styles={styles.input}
+            // value={}
+            // onChangeText={}
+          />
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
