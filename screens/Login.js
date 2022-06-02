76a57659c0207f535666406b745df73647c899c0
Samuel Agbenyo <80902426+Sonnysam@users.noreply.github.com>
2022-06-02 11:29:35 +0000

    text inputs and login button added

--- screens/Login.js
+++ screens/Login.js
@@ -4,26 +4,53 @@ import {
   View,
   TextInput,
   KeyboardAvoidingView,
+  TouchableOpacity,
 } from "react-native";
 import React from 'react'
-// import KeyboardAvoidingView from 'react-native/Libraries/Components/Keyboard/KeyboardAvoidingView'
+
 
 export default function Login() {
   return (
-    <KeyboardAvoidingView
-        style={styles.container}
-        behavior="padding"
-    >
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputCon}>
-          <TextInput
-            placeholder="Email"
-            styles={styles.input}
-            // value={}
-            // onChangeText={}
-          />
+        <TextInput
+          placeholder="Email"
+          styles={styles.input}
+          // value={}
+          // onChangeText={}
+        />
+        <TextInput
+          placeholder="Password"
+          styles={styles.input}
+          // value={}
+          // onChangeText={}
+          secureTextEntry
+        />
+
+        {/* Button Section */}
+        <View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {}}
+          >
+            <Text style={styles.button}>Login</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </KeyboardAvoidingView>
-  )
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputCon: {
+
+  },
+  input: {
+
+  },
+});
